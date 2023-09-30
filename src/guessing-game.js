c4289c5f2dc770ebf3a7f4c008590d3905b6e1af
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-09-30 12:06:50 +0300

    feat: implement functionality

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.min + this.max)/2);
+        return this.result; 
     }
-
+    
     lower() {
-
+        this.max = this.result
     }
-
+    
     greater() {
-
+        this.min = this.result
     }
-}
+};
+
+const game = new GuessingGame();
+
 
 module.exports = GuessingGame;
